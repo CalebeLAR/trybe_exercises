2194f037bddf5d9cbe262228da04c80cee3c56cf
calebeLAR <ca6ele@gmail.com>
2024-03-11 15:45:51 -0300

    feat: usa alert do sweetalert2

--- src/utils/hero_functions.js
+++ src/utils/hero_functions.js
@@ -1,3 +1,5 @@
+import Swal from "sweetalert2";
+
 // Elemento de imagem da API de super-heróis
 const hero_imgEL = document.querySelector("#hero-img");
 
@@ -16,9 +18,15 @@ export const choseImage = (image) => {
  * @throws {Error} - Lança um erro se a solicitação à API falhar.
  */
 export const fetch_superHero = (id) =>
-    new Promise((res, rej) =>
+    new Promise((res) =>
         fetch(`https://akabab.github.io/superhero-api/api/id/${id}.json`)
             .then((request) => request.json())
             .then((data) => res(data.images.sm))
-            .catch((err) => rej(err.message))
+            .catch(() => {
+                Swal.fire({
+                    text: "Opa, Ficamos sem heróis XD",
+                    icon: "question",
+                    confirmButtonText: "tudo bem eu gero outro..",
+                });
+            })
     );
