f2e470c6bf7eed5cda687fdf64e1bb1f36552f9a
calebeLAR <ca6ele@gmail.com>
2025-03-02 19:08:13 -0300

    simpsons

--- my-scripts/simpsons.js
+++ my-scripts/simpsons.js
@@ -19,9 +19,24 @@ const findSimposon = (id) => {
 };
 
 // C - Crie uma função que altere o arquivo simpsons.json retirando os personagens com id 10 e 6.
-
-const newData = simpsons.filter((simpson) => !["10", "6"].includes(simpson.id));
+var newData = simpsons.filter((simpson) => !["10", "6"].includes(simpson.id));
 fs.writeFileSync('my-scripts/simpsons.json', JSON.stringify(newData))
 console.log(newData)
 
+// D - Crie uma função que leia o arquivo simpsons.json e crie um novo arquivo, 
+// chamado simpsonFamily.json, contendo as personagens com id de 1 a 4.
+var newData = simpsons.filter((simpson) => simpson.id <= 4);
+fs.writeFileSync('my-scripts/simpsonFamily.json', JSON.stringify(newData))
+console.log(newData)
 
+// E - Crie uma função que adicione ao arquivo simpsonFamily.json o personagem Nelson Muntz.
+var newId = simpsons.reduce((acc, simpson) => (acc < simpson.id) ? acc = +simpson.id + 1 : acc, 0);
+var newData = [...simpsons, { 'id': `${newId}`, 'simpson': 'Nelson Muntz' }]
+fs.writeFileSync('my-scripts/simpsonFamily.json', JSON.stringify(newData))
+
+// F - Crie uma função que substitua o personagem Nelson Muntz pela personagem
+// Maggie Simpson no arquivo simpsonFamily.json.
+const condition = (simpson) => "Nelson Muntz" == simpson.name ? { ...simpson, name: "Maggie Simpson" } : simpson
+var newData = simpsons.map((simpson) => condition(simpson));
+fs.writeFileSync('my-scripts/simpsons.json', JSON.stringify(newData))
+console.log(newData)
