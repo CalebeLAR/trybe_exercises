d62e028e5013f83fd7d9c03505d94e3447d0413e
calebeLAR <ca6ele@gmail.com>
2025-03-12 10:36:54 -0300

    Criar um endpoint do tipo GET com a rota /movies/:id, que possa listar um filme do JSON por id

--- src/app.js
+++ src/app.js
@@ -3,18 +3,26 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const readMovies = async () => {
+    const movies = await fs.readFile('src/movies.json', 'utf-8');
+
+    return JSON.parse(movies)
+}
+
 app.get('/', (req, res) => {
-  res.send('Olá Mundo!')
+    res.send('Olá Mundo!')
+})
+
+app.get('/movies/:id', async (req, res) => {
+    const movieId = req.params.id
+    const movies = await readMovies()
+    const movie = movies.find((movie)=> movie.id == movieId)
+
+
+    res.send(movie)
 })
 
 app.listen(port, () => {
-  console.log(`App de exemplo esta rodando na porta ${port}`)
+    console.log(`App de exemplo esta rodando na porta ${port}`)
 })
 
-const readFile = async () => {
-    const movies = await fs.readFile('src/movies.json', 'utf-8');
-
-    console.log(movies)
-}
-
-readFile()
